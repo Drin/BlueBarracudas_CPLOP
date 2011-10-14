851420abbced81773c5d14a0ef5e7b6b9f590c5b
Aldrin <DrinMontana@gmail.com>
2011-10-14 16:22:44 -0700

    Modified the xml parser to grab more values from the xml file. There will need to be more changes in the future and losing these changes would suck terribly

--- xmlFunctions.php
+++ xmlFunctions.php
@@ -13,6 +13,10 @@ if (!isset($__XMLPARSER_FUNCTIONS__PHP))
 
    $wellIdMap = array();
    $peakValueMap = array();
+   $peakAreaMap = array();
+   $peakWidthMap = array();
+   $baselineOffsetMap = array();
+   $signalToNoiseMap = array();
    $dispSeqMap = array();
    $normalizedValMap = array();
    $wellDropOffCurves = array();
@@ -22,7 +26,8 @@ if (!isset($__XMLPARSER_FUNCTIONS__PHP))
    ################################
 
    function tagData($parser, $data) {
-      global $dispTag, $currTag, $currWellId, $wellIdMap, $dispSeqMap, $normalizedValMap, $peakValueMap;
+      global $dispTag, $currTag, $currWellId, $wellIdMap, $dispSeqMap, $normalizedValMap, $peakValueMap,
+       $peakAreaMap, $peakWidthMap, $baselineOffsetMap, $signalToNoiseMap;
 
       if ($dispTag == "DISPENSATION")  {
          //echo "dispTag says dispensation, ";
@@ -41,16 +46,62 @@ if (!isset($__XMLPARSER_FUNCTIONS__PHP))
          $wellIdMap[$currWellId] = $data;
       }
 
-      else if ($dispTag == "DISPENSATION" && $currTag == "SIGNALVALUE" && $currWellId != "") {
-         //echo "added peakValue Well: $currWellId, ";
-         if (!isset($peakValueMap[$currWellId])) {
-            $peakValueMap[$currWellId] = array();
+      else if ($dispTag == "DISPENSATION" && $currWellId != "") {
+         if ($currTag == "SIGNALVALUE") {
+            //echo "added peakValue Well: $currWellId, ";
+            if (!isset($peakValueMap[$currWellId])) {
+               $peakValueMap[$currWellId] = array();
+            }
+         
+            $peakList = $peakValueMap[$currWellId];
+            $peakList[count($peakList)] = $data;
+
+            $peakValueMap[$currWellId] = $peakList;
          }
+         else if ($currTag == "PEAKAREA") {
+            //echo "added peakArea Well: $currWellId, ";
+            if (!isset($peakAreaMap[$currWellId])) {
+               $peakAreaMap[$currWellId] = array();
+            }
+         
+            $peakList = $peakAreaMap[$currWellId];
+            $peakList[count($peakList)] = $data;
 
-         $peakList = $peakValueMap[$currWellId];
-         $peakList[count($peakList)] = $data;
+            $peakAreaMap[$currWellId] = $peakList;
+         }
+         else if ($currTag == "PEAKWIDTH") {
+            //echo "added peakWidth Well: $currWellId, ";
+            if (!isset($peakWidthMap[$currWellId])) {
+               $peakWidthMap[$currWellId] = array();
+            }
+         
+            $peakList = $peakWidthMap[$currWellId];
+            $peakList[count($peakList)] = $data;
 
-         $peakValueMap[$currWellId] = $peakList;
+            $peakWidthMap[$currWellId] = $peakList;
+         }
+         else if ($currTag == "BASELINEOFFSET") {
+            //echo "added baselineOffset Well: $currWellId, ";
+            if (!isset($baselineOffsetMap[$currWellId])) {
+               $baselineOffsetMap[$currWellId] = array();
+            }
+         
+            $peakList = $baselineOffsetMap[$currWellId];
+            $peakList[count($peakList)] = $data;
+
+            $baselineOffsetMap[$currWellId] = $peakList;
+         }
+         else if ($currTag == "SIGNALTONOISE") {
+            //echo "added signalToNoise Well: $currWellId, ";
+            if (!isset($signalToNoiseMap[$currWellId])) {
+               $signalToNoiseMap[$currWellId] = array();
+            }
+         
+            $peakList = $signalToNoiseMap[$currWellId];
+            $peakList[count($peakList)] = $data;
+
+            $signalToNoiseMap[$currWellId] = $peakList;
+         }
       }
    }
 
@@ -152,6 +203,54 @@ if (!isset($__XMLPARSER_FUNCTIONS__PHP))
       }
    }
 
+   function printPeakAreas() {
+      global $peakAreaMap;
+
+      foreach ($peakAreaMap as $wellId => $peakList) {
+         echo "well $wellId has peaks areas:\n";
+
+         foreach ($peakList as $ndx => $peakArea) {
+            echo "\tindex $ndx = $peakArea:\n";
+         }
+      }
+   }
+
+   function printPeakWidths() {
+      global $peakWidthMap;
+
+      foreach ($peakWidthMap as $wellId => $peakList) {
+         echo "well $wellId has peaks widths:\n";
+
+         foreach ($peakList as $ndx => $peakWidth) {
+            echo "\tindex $ndx = $peakWidth:\n";
+         }
+      }
+   }
+
+   function printBaselineOffsets() {
+      global $baselineOffsetMap;
+
+      foreach ($baselineOffsetMap as $wellId => $peakList) {
+         echo "well $wellId has baseline offsets:\n";
+
+         foreach ($peakList as $ndx => $baselineOffset) {
+            echo "\tindex $ndx = $baselineOffset:\n";
+         }
+      }
+   }
+
+   function printSignalToNoises() {
+      global $signalToNoiseMap;
+
+      foreach ($signalToNoiseMap as $wellId => $peakList) {
+         echo "well $wellId has signal to noises:\n";
+
+         foreach ($peakList as $ndx => $signalToNoise) {
+            echo "\tindex $ndx = $signalToNoise:\n";
+         }
+      }
+   }
+
    ################################
    ########### Main ###############
    ################################
@@ -169,6 +268,14 @@ if (!isset($__XMLPARSER_FUNCTIONS__PHP))
       while ($xmlFileData = fread($file, filesize($fileName))) {
          $success = xml_parse($xmlParser, $xmlFileData);
       }
+
+      /*
+      printPeakAreas();
+      printPeakWidths();
+      printBaselineOffsets();
+      printSignalToNoises();
+       */
+      printWellIds();
 
       $basicFileName = preg_replace('/\.pyrorun$/', '', $shortName);
       
@@ -215,7 +322,13 @@ if (!isset($__XMLPARSER_FUNCTIONS__PHP))
          #$normArr = $normalizedValMap[$wellId];
          //echo "wellID as a key: $wellId, ";
          $peakArr = $peakValueMap[$wellId];
+         $peakWidthArr = $peakWidthMap[$wellId];
+         $peakAreaArr = $peakAreaMap[$wellId];
+         $baselineOffsetArr = $baselineOffsetMap[$wellId];
+         $signalToNoiseArr = $signalToNoiseMap[$wellId];
 
+         #TODO add appropriate attribute names to dataPointQuery string and insert pyroprint values
+         #in the same format as peakArr[$pos] (the form <valueMap[$pos]>)
          foreach ($normalizedValMap[$wellId] as $pos => $normVal) {
             $dataPointQuery = "INSERT IGNORE INTO pyrogram_data_point (pyrogram_num, position, peak_value,"
                              ."compensated_peak_value, nucleotide) VALUES ($pyroNum, $pos, $peakArr[$pos], "
@@ -237,4 +350,5 @@ if (!isset($__XMLPARSER_FUNCTIONS__PHP))
       }
    }
 }
+parseXMLFile($argv[1], "");
 ?>
